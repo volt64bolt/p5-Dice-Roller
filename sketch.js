cae1fadefb1a3c82fc05d37e71bc9cecc0542c60
volt64bolt <81176807+volt64bolt@users.noreply.github.com>
2023-03-22 17:13:57 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,11 +1,11 @@
-let img;
 let imgs;
 let click;
 let count;
 let blacklist;
-let num;
+let nums;
 let rolled;
 let myFont;
+let rdm
 
 function preload() {
     myFont = loadFont('assets/Roboto-Medium.ttf');
@@ -14,47 +14,30 @@ function preload() {
 function setup() {
   createCanvas(windowWidth, windowHeight, WEBGL);
   
-  img = loadImage('assets/dice50.jpg');
   click = 0;
-  count = 50; //total amount of numbers
-  blacklist = [25, 33, 41]; //numbers you dont want
+  count = 44; //total amount of numbers
+  blacklist = [9]; //numbers you dont want
 
   imgs = [];
-  for (let i = 0; i <= count; i++) {
+  for (let i = 0; i <= count + 1; i++) {
     imgs[i] = loadImage('assets/dice' + str(i) + '.jpg');
   }
 
-  num = 0;
+  nums = [0, 0, 0];
   rolled = [];
+
+  rdm = [imgs[0], imgs[0], imgs[0]];
+
   textFont(myFont);
-  textSize(50);
+  textSize(75);
 }
 
 function draw() {
   background(220);
-  
-  //text('potato', -(windowWidth/2) + 50, -(windowHeight/2) + 80)
+  drawDice(0, -200, 0);
+  drawDice(1, 0, 0);
+  drawDice(2, 200, 0);
 
-  if (click == 0) {
-      // translate(windowWidth/2, windowHeight/2, 0);
-      push();
-    //   img = loadImage('assets/dice' + str(ceil(random(count))) + '.jpg');
-      texture(imgs[ceil(random(count))]);
-      rotateX(frameCount * -0.2);
-      box(170, 170, 170);
-      pop();
-    }
-  else if (click == 1) {
-      push();
-    //   img = loadImage('assets/dice' + str(num) + '.jpg');
-      texture(imgs[num]);
-      rotateX(0);
-      box(170, 170, 170);
-      pop();
-    }
-  
-    
-    updateText()
 }
 
 function windowResized() {
@@ -65,17 +48,17 @@ function mousePressed() {
   if (click == 0) {
     click = 1;
 
-    num = ceil(random(count)) + 1;
+    for (let i = 0; i < 3; i++) {
+      nums[i] = ceil(random(count)) + 1;
 
-    while (blacklist.includes(num)) {
-        console.log('re rolling. Num is:', num)
-        num = ceil(random(count)) + 1;
+      while (blacklist.includes(nums[i])) {
+          console.log('re rolling. Num was:', nums[i])
+          nums[i] = ceil(random(count)) + 1;
+      }
+
+      console.log('num is:', nums[i])
+      rolled.push(nums[i]);
     }
-
-
-    console.log('num is:', num)
-    rolled.push(num);
-    
   }
 
   else if (click == 1) {
@@ -87,24 +70,42 @@ function mousePressed() {
     else if (rolled.length <3) {
         click = 0;
     }
-
   }
 
   else if (click == 2) {
     click = 0;
     rolled = [];
-    
   }
-
-
-
 }
 
 
 function updateText() {
     //console.log('updating text')
     for (let i = 0; i < rolled.length; i++) {
-        text(rolled[i], -(windowWidth/2) + 50, -(windowHeight/2) + 80 + 50*i);
+        text(rolled[i], -(windowWidth/2) + 50, -(windowHeight/2) + 100 + 75*i);
         //console.log('updated text')
     }
+}
+
+function drawDice(c, x, y) {
+    if (click == 0) {
+      push();
+      translate(x, y, 0);
+      if ((frameCount * -5) % 90 == 0) {
+        rdm[c] = imgs[ceil(random(count))];
+      }
+      texture(rdm[c])
+      rotateX(radians(frameCount * -5));
+      box(170, 170, 170);
+      pop();
+    }
+  else if (click == 1) {
+      push();
+      translate(x, y, 0);
+      texture(imgs[nums[c]]);
+      rotateX(0);
+      box(170, 170, 170);
+      pop();
+    }
+    updateText()
 }
